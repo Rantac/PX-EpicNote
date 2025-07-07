3033805cc7841c233e78bb45044786eab03b5817
kinda yin <kindayin12@gmail.com>
2025-07-07 08:35:15 +0000

    I want to change dollar icon in navigation please use solid icon

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 
 const EpicNotesIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
-        <path d="M116,80h4v40h-4a20,20,0,0,1,0-40Zm32,56H136v40h12a20,20,0,0,0,0-40Zm84-8A104,104,0,1,1,128,24,104.11,104.11,0,0,1,232,128Zm-48,28a36,36,0,0,0-36-36H136V80h4a20,20,0,0,1,20,20,8,8,0,0,0,16,0,36,36,0,0,0-36-36h-4V56a8,8,0,0,0-16,0v8h-4a36,36,0,0,0,0,72h4v40h-8a20,20,0,0,1-20-20,8,8,0,0,0-16,0,36,36,0,0,0,36,36h8v8a8,8,0,0,0,16,0v-8h12A36,36,0,0,0,184,156Z"></path>
+        <path d="M232,48V200a16,16,0,0,1-16,16H88a16,16,0,0,1-16-16V48A16,16,0,0,1,88,32H216A16,16,0,0,1,232,48ZM64,48V200H48a8,8,0,0,1,0-16H56V56H48a8,8,0,0,1,0-16Zm152-8H88a8,8,0,0,0-8,8V200a8,8,0,0,0,8,8H216a8,8,0,0,0,8-8V48A8,8,0,0,0,216,40Z"></path>
     </svg>
 );
 
@@ -26,7 +26,7 @@ export function BottomNav() {
   const pathname = usePathname();
 
   return (
-    <nav className="flex gap-2 border-t border-border bg-background px-4 py-2">
+    <nav className="flex gap-2 border-t border-border bg-background px-4 py-2 h-[68px]">
         {navItems.map((item) => {
           const isActive = pathname === item.href;
           return (

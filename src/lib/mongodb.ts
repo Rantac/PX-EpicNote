b9f9759d5197db19d4f10519ae652c290f801ec5
kinda yin <kindayin12@gmail.com>
2025-07-11 09:24:27 +0000

    add temp

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -25,3 +25,4 @@ if (process.env.NODE_ENV === 'development') {
 
 // Export a MongoClient promise. By doing this in a separate module, the client can be shared across functions.
 export default clientPromise;
+
